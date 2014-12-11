16d506fd9b780aaee047577178428c1997dbc9b6
JCS PRO <JCS-PRO@iMac-de-JCS.local>
2014-12-11 16:29:16 +0100

    installation

---
+++ src/toolbox.js
@@ -0,0 +1,42 @@
+(function(window, document, undefined) {
+    'use strict';
+    /*
+     * Global api.
+     */
+    var toolbox = window.toolbox = {
+        get: function() {
+            return _instance;
+        },
+        //Main entry point.
+        init: function(options) {
+            return _instance || new Transition(options);
+        }
+    };
+
+    /**
+     * Constructor.
+     */
+    function Toolbox(options) {
+        _instance = this;
+        return _instance;
+    }
+
+    Toolbox.prototype.getController = function()
+    {
+        return document.getElementById('main-content').getAttribute("data-controller")
+    }
+
+    Toolbox.prototype.getController = function()
+    {
+        var index = $(document.getElementById('state-indicator')).css('z-index');
+            var states = {
+                1 : 'mobile',
+                2:  'tablet'
+            };
+            return states[index] || 'desktop';
+    }
+
+    // Singleton
+    var _instance;
+
+}(window, document));
